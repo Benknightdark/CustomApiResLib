2fbf497a0f9bc38b3d20ac5be3603433f682bb51
moon791017 <moon791017@gmail.com>
2020-05-07 12:19:39 +0800

    OK

---
+++ CutomApiLib/Middlewares/CustomResponseResult.cs
@@ -0,0 +1,46 @@
+﻿using CutomApiLib.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CutomApiLib.Middlewares
+{
+    public class CustomResponseResult : IActionFilter
+    {
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+
+            if (context.Result is ObjectResult objectResult)
+            {
+
+                if (objectResult.StatusCode != 200)
+                {
+                    objectResult.Value = new CustomResponseModel
+                    {
+                        status = (int)objectResult.StatusCode,
+                        title = objectResult.StatusCode.ToString(),
+                        errors = objectResult.Value
+                    };
+                }
+                else
+                {
+                    objectResult.Value = new CustomResponseModel
+                    {
+                        status = (int)objectResult.StatusCode,
+                        data = objectResult.Value
+                    };
+                }
+
+            }
+        }
+
+        void IActionFilter.OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+
+
+    }
+}
