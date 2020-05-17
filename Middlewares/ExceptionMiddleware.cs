3854a72f3dd9adb75c3a6f687e11bec6ef7f9424
BenKung <moon791017@gmail.com>
2020-05-17 16:53:14 +0800

    OK

--- Middlewares/ExceptionMiddleware.cs
+++ Middlewares/ExceptionMiddleware.cs
@@ -1,34 +1,30 @@
-﻿using CutomApiLib.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
+using CutomApiLib.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
-namespace CutomApiLib.Middlewares
-{
-    public static class ExceptionMiddleware
-    {
-        public static void UseCustomExceptionMiddleware(this IApplicationBuilder app)
-        {
-            app.UseExceptionHandler(errorApp =>
-            {
-                errorApp.Run(async context =>
-                {
-                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+namespace CutomApiLib.Middlewares {
+    public static class ExceptionMiddleware {
+        public static void UseCustomExceptionMiddleware (this IApplicationBuilder app) {
+            app.UseExceptionHandler (errorApp => {
+                errorApp.Run (async context => {
+                    context.HandleResponse ();
+
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature> ();
                     context.Response.StatusCode = 500;
-                    context.Response.ContentType = new MediaTypeHeaderValue("application/json").ToString();
+                    context.Response.ContentType = new MediaTypeHeaderValue ("application/json").ToString ();
                     string ErrorContent = exceptionHandlerPathFeature?.Error?.Message + " => " + exceptionHandlerPathFeature?.Error.StackTrace;
-                    var bodyContent = new CustomResponseModel
-                    {
+                    var bodyContent = new CustomResponseModel {
                         status = 500,
-                        title = context.Response.StatusCode.ToString(),
-                        errors=ErrorContent                       
+                        title = context.Response.StatusCode.ToString (),
+                        errors = ErrorContent
                     };
-                    await context.Response.WriteAsync(JsonSerializer.Serialize(bodyContent), Encoding.UTF8);
+                    await context.Response.WriteAsync (JsonSerializer.Serialize (bodyContent), Encoding.UTF8);
                 });
             });
         }
